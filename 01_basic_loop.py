03619e6dcc6290d14a8f9aad12d8e02a8a8a379a
TerrenceWu14 <135181592+TerrenceWu14@users.noreply.github.com>
2023-06-09 10:38:02 +1200

    Add files via upload

--- 01_basic_loop.py
+++ 01_basic_loop.py
@@ -1,11 +1,23 @@
+# checks if the user entered a number that is more than zero
 valid = False
 while not valid:
 
-    response = float(input("Enter a number: "))
+    error = "Please enter a number that is more than zero"
 
-    if response > 0:
-        valid = True
+    try:
 
-    else:
-        print("Please enter a number that is more than zero")
-        print()
+        # asks user to enter a number
+        response = float(input("Enter a number: "))
+
+        # checks if a number is above zero
+        if response > 0:
+            valid = True
+
+        # outputs error if number is invalid
+        else:
+            print(error)
+            print()
+
+    except ValueError:
+        print(error)
+
