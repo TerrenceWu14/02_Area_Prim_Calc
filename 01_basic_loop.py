c33c06ab00fd6e5f685175b871dc7fa39cd6197f
TerrenceWu14 <135181592+TerrenceWu14@users.noreply.github.com>
2023-06-09 11:04:53 +1200

    Add files via upload

--- 01_basic_loop.py
+++ 01_basic_loop.py
@@ -1,23 +1,41 @@
-# checks if the user entered a number that is more than zero
-valid = False
-while not valid:
+# function goes here
 
-    error = "Please enter a number that is more than zero"
+# checks input is a number more than zero
+def num_check(question):
+    # checks if the user entered a number that is more than zero
+    valid = False
+    while not valid:
 
-    try:
+        error = "Please enter a number that is more than zero"
 
-        # asks user to enter a number
-        response = float(input("Enter a number: "))
+        try:
 
-        # checks if a number is above zero
-        if response > 0:
-            valid = True
+            # asks user to enter a number
+            response = float(input(question))
 
-        # outputs error if number is invalid
-        else:
+            # checks if a number is above zero
+            if response > 0:
+                return response
+
+            # outputs error if number is invalid
+            else:
+                print(error)
+                print()
+
+        except ValueError:
             print(error)
-            print()
 
-    except ValueError:
-        print(error)
 
+# Main routine goes here
+width = num_check("Width: ")
+height = num_check("Height: ")
+
+# calculate the area (width x height)
+area = width * height
+
+# calculate the perimeter (width + height) x 2
+perimeter = 2 * (width + height)
+
+# Output area and perimeter
+print("Perimeter: {} units" .format(perimeter))
+print("Area: {} units" .format(area))3
