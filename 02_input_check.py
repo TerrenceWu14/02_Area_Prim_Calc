8fb430698ee8a50faefd6f3900297e5978e48b5f
TerrenceWu14 <135181592+TerrenceWu14@users.noreply.github.com>
2023-06-29 12:26:33 +1200

    Add files via upload

--- 02_input_check.py
+++ 02_input_check.py
@@ -1,20 +1,34 @@
 # checks user choice is 'integer', 'text' or 'image'
 def user_choice():
-
     valid = False
+
     while not valid:
         # asks user for choice and change response to lowercase
         response = input("File type (integer / text / image):").lower()
 
-
-        # If they choose "t" or "text", return "text"
+        # Lists of valid responses
         text_ok = ["text", "t", "txt"]
+        integer_ok = ["integer", "int", "#" "number"]
+        image_ok = ["image", "img", "pix", "picture", "pic", "p"]
+
+        # Checks for valid response and returns text, integer or image
         if response in text_ok:
             return "text"
+        elif response in integer_ok:
+            return "integer"
+        elif response in image_ok:
+            return "image"
+        elif response == "i":
+            want_integer = input("Press <enter> for an inter or any key for an image: ")
+
+            if want_integer == "":
+                return "integer"
+            else:
+                return "image"
 
         else:
             # if response is not valid, output an error
-            print("Please choose a valid file type!")
+            print("Sorry, please choose integer, text or image")
             print()
 
 
