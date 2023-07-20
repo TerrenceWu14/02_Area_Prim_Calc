d5671e8a931151f0287e508484817c192c7cdc7c
TerrenceWu14 <135181592+TerrenceWu14@users.noreply.github.com>
2023-07-21 10:48:06 +1200

    Add files via upload

--- 06_integer_bits.py
+++ 06_integer_bits.py
@@ -23,3 +23,25 @@ def num_check(question, low):
         except ValueError:
             print(error)
 
+# Converts decimal to binary and states how
+# Many bits are needed to represent the original integer
+def int_bits():
+
+    # Get integer (must be >=0)
+    var_integer = num_check("Please enter an integer", 0)
+
+    var_binary = "{0:b}".format(var_integer)
+
+    # Calculates # of bits (length of string above)
+    num_bits = len(var_binary)
+
+        # Output answer with working out
+    print()
+    print("{} in binary is {}".format(var_integer, var_binary))
+    print("# of bits is {}".format(num_bits))
+    print()
+
+    return ""
+
+# Main routine
+int_bits()
